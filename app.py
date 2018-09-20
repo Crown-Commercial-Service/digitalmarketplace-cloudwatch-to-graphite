f9ffa6b56728397fece9b5e3d7bae10efcce7bbc
benvand <benjaminfvandersteen@gmail.com>
2018-09-20 08:50:46 +0100

    158-simplify-cloudwatch-to-graphite-app-remove-leadbutt-dependency
    Refactor/ simplification of the app:
    
    * Remove `pipe-watchdog.py` entirely
      * We're no longer using pipes to direct the output. An error should be output to the cloudwatch error logs on failure `cf logs --recent digitalmarketplace-cloudwatch-to-graphite-preview`
      * Remove this from the run command in `manifest.yml.example`
    
    * Add `flake8` dependency and `.flake8` file in line with GDS recommendations
    * Activate travis service on repo to check Flake8 rules and requirements files
      * Add `.travis.yml`
    
    * Add `Makefile` with shortcuts for running requirements freeze and flake8
    * Update `README.md`
    * Change requirements handling
      * Add `requirements-app.txt`
      * Ensure `requirements.txt` contains full pinned requirements
      * Add `flake8` requirement for devs
    
    * Rewrite `app.py`
      * Add function descriptions to all functions
      * Remove dependency on `leadbutt`, `signal` and `subprocess`
      * Use `boto3` instead of `leadbutt`s `boto` (https://github.com/crccheck/cloudwatch-to-graphite/blob/master/leadbutt.py#L25)
      * Remove `get_timestamp` method. The same can be achieved with `time.time`
      * `logger.warn` is deprecated, use `logger.warning` instead
      * Bring string formatting in line with rest of strings in file
      * Rename `initialize_metrics ` to `create_hostedgraphite_base_metrics` for better description
      * Remove `call_leadbutt` function and replace with code calling aws ourselves. Extrapolated from (https://github.com/crccheck/cloudwatch-to-graphite/blob/master/leadbutt.py)
      *  Change `loglevel` to `INFO` this is all that is required now we're not piping output
      * Move retry decorator on to wrapper method in `if __name__` block
    
    * Update `config.yaml.j2` template
      * `Dimensions` default = `{}` to avoid type manipulations
      * Remove `Auth` section from config. This can be hard coded until we have any need to specify it as a config variable
      * Remove `Options.Count` again, this can be hard coded. It's only used in one place and we've never needed to change it
    
    Extrapolate code out of leadbutt
    This code drops leadbutts retry functionality. Boto3 has the capacity to retry failed attempts. We should prefer this.

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-import os
-import signal
-import subprocess
 import logging
-from datetime import datetime, timedelta
+import os
+import sys
 
+from datetime import datetime, timedelta
+import boto3
 import requests
 import yaml
 from retrying import retry
@@ -13,58 +13,113 @@ from retrying import retry
 logger = logging.getLogger("app")
 
 
-def get_timestamp():
-    return (
-        (datetime.now() - timedelta(days=5)) - datetime(1970, 1, 1)
-    ).total_seconds()
+def get_config():
+    """Get the config file."""
+    with open('config.yaml') as fp:
+        config = yaml.load(fp)
+    return config
 
 
 def send_to_hostedgraphite(metrics):
+    """Put request to hostedgraphite."""
     response = requests.put(
         "http://www.hostedgraphite.com/api/v1/sink",
         auth=(os.getenv("HOSTED_GRAPHITE_API_KEY"), ""),
         data=metrics
     )
 
-    if response.status_code >= 400:
-        logger.warn(f"Error sending metrics to hosted graphite - Status code {response.status_code}")
+    if not response.ok:
+        logger.warning(
+            "Error sending metrics to hosted graphite - {}: {}".format(response.status_code, response.reason)
+        )
     else:
-        logger.info(f"Metrics sent to hosted graphite - Status code {response.status_code}")
+        logger.info("Metrics sent to hosted graphite - Status code {}".format(response.status_code))
 
 
-def initialize_metrics():
-    initialized_metrics = []
-    timestamp = int(get_timestamp())
-    with open('config.yaml') as fp:
-        config = yaml.load(fp)
-        for metric in config['Metrics']:
-            stats = metric['Statistics'] if isinstance(metric['Statistics'], list) else [metric['Statistics']]
-            for stat in stats:
-                metric_name = (metric['Options']['Formatter']
-                               .replace("%(statistic)s", stat.lower()))
-                initialized_metrics.append("{} 0.0 {}".format(metric_name, timestamp))
-    send_to_hostedgraphite("\n".join(initialized_metrics))
+def format_config_metric_entry_for_hostedgraphite_base_metric(config_metric_entry, timestamp):
+    hostedgraphite_metric_name = config_metric_entry['Options']['Formatter'].replace(
+        "%(statistic)s",
+        config_metric_entry['Statistics'].lower()
+    )
+    hostedgraphite_base_metric = "{} 0.0 {}".format(hostedgraphite_metric_name, timestamp)
+    return hostedgraphite_base_metric
 
 
-@retry(wait_fixed=60000, retry_on_result=lambda res: res is None)
-def call_leadbutt():
+def create_hostedgraphite_base_metrics(config):
+    """Take a metric entry from the config format it into a base metric used to initialise the metric on hostedgraphite.
     """
-    See https://github.com/crccheck/cloudwatch-to-graphite for more info on leadbutt
+    base_metrics = []
+    timestamp = int(((datetime.now() - timedelta(days=5)) - datetime(1970, 1, 1)).total_seconds())
+    for config_metric_entry in config['Metrics']:
+        hostedgraphite_base_metric = format_config_metric_entry_for_hostedgraphite_base_metric(
+            config_metric_entry,
+            timestamp
+        )
+        hostedgraphite_base_metrics.append(hostedgraphite_base_metric)
+    return hostedgraphite_base_metrics
+
+
+def format_cloudwatch_metric_datapoint_for_hostedgraphite(metric, result):
+    """Given a cloudwatch metric datapoint convert it into the format hostedgraphite expects."""
+    hostedgraphite_metric_name = (
+        cloudwatch_metric_datapoint['Options']['Formatter'] % {'statistic': cloudwatch_metric_datapoint['Statistics']}
+    ).lower()
+    return '{0} {1} {2}'.format(
+        hostedgraphite_metric_name,
+        result[cloudwatch_metric_datapoint['Statistics']],
+        result['Timestamp'].strftime('%s')
+    )
+
+
+def get_metric_from_cloudwatch(client, metric):
+    """Call the client once for th supplied metric."""
+    end_time = datetime.utcnow()
+    start_time = end_time - timedelta(seconds=600)
+    return client.get_metric_statistics(
+        Period=60,
+        StartTime=start_time,
+        EndTime=end_time,
+        MetricName=metric['MetricName'],
+        Namespace=metric['Namespace'],
+        Statistics=[metric['Statistics']],
+        Dimensions=[{'Name': k, 'Value': v} for k, v in metric['Dimensions'].items()],
+        Unit=metric.get('Unit', 'None'),
+    )
+
+
+def get_metrics_from_cloudwatch_and_format_for_hostedgraphite(config):
+    """Get metrics from config, call cloudwatch for metric entry and format metric datapoints to expected hostedgraphite
+    format.
+
+    For each metric in the config call the cloudwatch api to return the datapoints for that metric.
+    For each cloudwatch datapoint create one hostedgraphite metric entry for supply to hostedgraphite.
     """
-    result = subprocess.Popen("leadbutt", stdout=subprocess.PIPE)
-    metrics = result.communicate()[0].decode("utf-8")
-    send_to_hostedgraphite(metrics)
+    hostedgraphite_metrics = []
+
+    client = boto3.client('cloudwatch', region_name="eu-west-1")
+    for metric_entry in config['Metrics']:
+        cloudwatch_metric = get_metric_from_cloudwatch(client, metric_entry)
+        for cloudwatch_metric_datapoint in cloudwatch_metric['Datapoints']:
+            hostedgraphite_metric = format_cloudwatch_metric_datapoint_for_hostedgraphite(
+                metric_entry,
+                cloudwatch_metric_datapoint
+            )
+            hostedgraphite_metrics.append(hostedgraphite_metric)
+
+    return hostedgraphite_metrics
 
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s:%(name)s:%(levelname)s:%(message)s",
-    )
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s:%(name)s:%(levelname)s:%(message)s", stream=sys.stdout)
+    config = get_config()
 
-    # python by default installs a signal handler that largely ignores SIGPIPE, but we want to use it for our watchdog
-    # mechanism, so reinstate the *unix* default, which is a fatal handler
-    signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    base_metrics = create_hostedgraphite_base_metrics(config)
+    send_to_hostedgraphite("\n".join(base_metrics))
 
-    initialize_metrics()
-    call_leadbutt()
+    @retry(wait_fixed=60000, retry_on_result=lambda res: res is None)
+    def sleep_and_send_retry():
+        """Wrapper to apply retry to get and send methods."""
+        hostedgraphite_metrics = get_metrics_from_cloudwatch_and_format_for_hostedgraphite(config)
+        send_to_hostedgraphite('\n'.join(hostedgraphite_metrics))
+
+    sleep_and_send_retry()
